5463a0a077d94408763ae6a1d351bee0b91b12cb
BakedSoups <overbakedrice@gmail.com>
2026-03-26 14:25:52 -0700

    restructured folders

--- wine_flavor/main.py
+++ wine_flavor/main.py
@@ -1,12 +1,14 @@
 
 import datasource
+import transforms 
+
+
 # grab all the wines from the first page 
 wines = datasource.fetch_vivino_wines(num_pages=1)
 # Example of running:  fetch_vivino_wines(page=1, num_pages=3)
 
 # get all unique flavors from wine
-unique_flavors = datasource.unique_flavors(wines) 
-
+unique_flavors = transforms.unique_flavors(wines) 
 
 
 print(wines.head())        # first 5 rows
--- wine_flavor/transforms/__init__.py
+++ wine_flavor/transforms/__init__.py
@@ -1,4 +1,4 @@
-from .vivino.vivino_fetch_flavors import fetch_vivino_wines
+from .vivino.vivino_clean_data import unique_flavors
 
 
 # all data packages for data sources MUST return a pandas data frame only 
